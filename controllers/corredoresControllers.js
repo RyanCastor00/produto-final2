db5cf319b7a1e64b73cab34076864237be2d9aec
RyanCastor00 <ryanmatheussoaresdearaujo@gmail.com>
2026-04-28 14:05:38 -0300

    oi

--- controllers/corredoresControllers.js
+++ controllers/corredoresControllers.js
@@ -17,7 +17,7 @@ exports.createCorredor = async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
-};
+}
 
 exports.updateCorredor = async (req, res) => {
   try {
